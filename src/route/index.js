c5c8532adebaac3321d35889a01c236bac750bf2
Tala261012 <tala261012@gmail.com>
2023-03-17 16:52:08 +0200

    Bootstrap Lesson 3

--- src/route/index.js
+++ src/route/index.js
@@ -691,11 +691,28 @@ router.get('/web', function (req, res) {
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/bootstrap', function (req, res) {
+router.get('/bootstrap2', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
   res.render('bootstrap2', {
+    layout: 'bootstrap',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/bootstrap3', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrap3', {
     layout: 'bootstrap',
   })
   //                  ↑↑ сюди вводимо JSON дані
